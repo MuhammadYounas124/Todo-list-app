ed1f82e99eddbed2c7ba4edacc456e14e92c96d6
MuhammadYounas124 <younasraja300@gmail.com>
2024-12-08 16:08:55 +0300

    change in main and add

--- todo-list-react-app/src/App.tsx
+++ todo-list-react-app/src/App.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-
-import Taskform from './Components-todo-app/Task-form';
+import React, { useState } from 'react';
+import TaskForm from './Components-todo-app/Task-form';
 import TodoList from './Components-todo-app/Todo-list';
 
+interface Task {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
 const App: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (task: Task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const completeTask = (index: number) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index: number) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <Provider store={store}>
-      <div className="container mt-4">
-        <h1>Task Manager</h1>
-        <Taskform />
-        <TodoList />
-      </div>
-    </Provider>
+    <div className="container mt-4">
+      <h1>Task Manager</h1>
+      <TaskForm addTask={addTask} />
+      <TodoList tasks={tasks} completeTask={completeTask} deleteTask={deleteTask} />
+    </div>
   );
 };
 
