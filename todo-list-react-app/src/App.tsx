45d6f3611379f220ea4bf514b40719ad6e459b31
MuhammadYounas124 <younasraja300@gmail.com>
2024-12-08 15:17:20 +0300

    delete redux folder

--- todo-list-react-app/src/App.tsx
+++ todo-list-react-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+
 import Taskform from './Components-todo-app/Task-form';
 import TodoList from './Components-todo-app/Todo-list';
 
