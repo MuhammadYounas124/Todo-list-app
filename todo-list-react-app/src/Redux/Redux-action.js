f54eca7eb2ea2013819110f11093455de692bd26
MuhammadYounas124 <younasraja300@gmail.com>
2024-12-16 16:15:56 +0300

    add action type of redux

---
+++ todo-list-react-app/src/Redux/Redux-action.js
@@ -0,0 +1,25 @@
+// actions.js
+
+// Define action types
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const RESET = 'RESET';
+
+// Action creators
+export const increment = () => {
+    return {
+        type: INCREMENT,
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: DECREMENT,
+    };
+};
+
+export const reset = () => {
+    return {
+        type: RESET,
+    };
+};
