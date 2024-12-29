966a7388321f549e0a8d69f4fb72c380d67bc446
MuhammadYounas124 <younasraja300@gmail.com>
2024-12-29 05:49:45 +0300

    add changes

--- todo-list-react-app/src/Redux/Redux-Reducer.ts
+++ todo-list-react-app/src/Redux/Redux-Reducer.ts
@@ -25,18 +25,23 @@ const rootReducer = (state: State = initialState, action: any): State => {
     case LOGIN: //Logs in a user
       return { ...state, user: action.payload }; // Updates the user field in the state with the data from action.payload.
     case LOGOUT:
-      return { ...state, user: null };
+      return { ...state, user: null }; // Sets the user field to null.
     case REGISTER:
-      return { ...state, user: action.payload };
+      return { ...state, user: action.payload }; // Updates the user field with the new user's data from action.payload.
     case ADD_TASK:
       return { ...state, tasks: [...state.tasks, action.payload] };
+      // Appends the new task (from action.payload) to the existing tasks array.
+      //  The ...state.tasks ensures the original tasks are preserved (immutability).
     case DELETE_TASK:
       return { ...state, tasks: state.tasks.filter((_, i) => i !== action.payload) };
+      //  Removes the task at the index specified in action.payload using the filter method. Immutability is maintained.
     case TOGGLE_TASK:
       return {
         ...state, // Returns a new state while ensuring immutability (using ...state and array methods like map and filter)
         tasks: state.tasks.map((task, i) => i === action.payload ? { ...task, completed: !task.completed } : task
-        ),
+        ), //  Toggles the completion status of a task. Uses map to iterate over the tasks array:
+//  If the current task’s index matches action.payload, it toggles the completed status (!task.completed).
+        // Otherwise, the task remains unchanged.Maintains immutability by creating a new array.
       };
     default:
       return state;
